b1b61e9207d4c0bb91547a366f45db397104e805
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-12-12 12:05:03 +0530

    initial

---
+++ src/main/java/com/example/nestdigitalAppnew_backend/NestdigitalAppnewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.nestdigitalAppnew_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestdigitalAppnewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestdigitalAppnewBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nestdigitalAppnew_backend/NestdigitalAppnewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nestdigitalAppnew_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestdigitalAppnewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
