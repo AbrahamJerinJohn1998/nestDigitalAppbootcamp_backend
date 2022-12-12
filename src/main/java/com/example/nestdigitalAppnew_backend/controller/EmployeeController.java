3b598c329de93ec628876e0facb7c69f8f031531
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-12-12 15:56:10 +0530

    securityguard controller

--- src/main/java/com/example/nestdigitalAppnew_backend/controller/EmployeeController.java
+++ src/main/java/com/example/nestdigitalAppnew_backend/controller/EmployeeController.java
@@ -1,15 +1,18 @@
 package com.example.nestdigitalAppnew_backend.controller;
 
+import com.example.nestdigitalAppnew_backend.dao.EmployeeDao;
 import com.example.nestdigitalAppnew_backend.model.Employee;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.nestdigitalAppnew_backend.model.LeaveCount;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 @RestController
 public class EmployeeController {
+    @Autowired
+    private EmployeeDao dao;
     @CrossOrigin("*")
     @PostMapping(path = "/add", consumes ="application/json",produces = "application/json")
     public Map<String,String> AddEmployee(@RequestBody Employee e)
@@ -21,10 +24,37 @@ public class EmployeeController {
         System.out.println(e.getAddress().toString());
         System.out.println(e.getUsername().toString());
         System.out.println(e.getPassword().toString());
-        dao.save(s);
+        dao.save(e);
+        HashMap<String,String> map=new HashMap<>();
+        map.put("status","success");
+        map.put("empid",String.valueOf(e.getId()));
+        return map;
+    }
+
+    @CrossOrigin(origins = "*")
+    @GetMapping("/view")
+    public List<Employee> ViewEmployee()
+    {
+        return (List<Employee>) dao.findAll();
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/search",consumes = "application/json",produces = "application/json")
+    public List<Employee> SearchEmployee(@RequestBody Employee e)
+    {
+        int empcode=e.getEmpcode();
+        System.out.println(empcode);
+        return (List<Employee>) dao.SearchEmployee(e.getEmpcode());
+    }
+    @CrossOrigin(origins = "*")
+    @PostMapping(path = "/delete",consumes = "application/json",produces = "application/json")
+    public Map<String, String> DeleteEmployee(@RequestBody Employee e)
+    {
+        String id=String.valueOf(e.getId());
+        System.out.println(id);
+        dao.DeleteEmployee(e.getId());
         HashMap<String,String> map=new HashMap<>();
         map.put("status","success");
         return map;
+
     }
-}
 }
---
+++ src/main/java/com/example/nestdigitalAppnew_backend/dao/SecurityGuardDao.java
@@ -0,0 +1,21 @@
+package com.example.nestdigitalAppnew_backend.dao;
+
+import com.example.nestdigitalAppnew_backend.model.Employee;
+import com.example.nestdigitalAppnew_backend.model.SecurityGuard;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface SecurityGuardDao extends CrudRepository<SecurityGuard,Integer> {
+
+    @Query(value = "SELECT `id`, `address`, `email`, `empcode`, `name`, `password`, `username` FROM `securityguard` WHERE `empcode` =:empcode",nativeQuery = true)
+    List<SecurityGuard> SearchSecurityGuard(@Param("empcode") int empcode);
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `securityguard` WHERE `id`= :id",nativeQuery = true)
+    void DeleteSecurityGuard(@Param("id") Integer id);
+}
